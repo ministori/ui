697844168d433c01411bb862abed8c7bd2c31bf1
ministori <ministori@gmail.com>
2016-12-27 16:36:24 +0900

    image rolling

--- js/function.js
+++ js/function.js
@@ -398,8 +398,12 @@ $(function(){
 
         $visual.hide().eq(0).show();
 
+        // 페이지 표시 함수 실행
+        pagination();
+
     }
 
+    // 다음 이미지 함수
     function fadeNext(){
         if( index >= visualLength ){ index = 0; }
 
@@ -408,8 +412,11 @@ $(function(){
 
         currentIndex = index;
         index++;
+1
+        currentPage();
     }
 
+    // 이전 이미지 함수
     function fadePrev(){
         if( index <= -1 ){ index = visualLength-1; }
 
@@ -418,6 +425,36 @@ $(function(){
 
         currentIndex = index;
         index--;
+
+        currentPage();
+    }
+
+    // 페이지 표시 함수
+    function pagination(){
+
+        for(var i=0; i<visualLength; i++){
+
+            var pageItem = '';
+            pageItem += '<li class="pagination-item">';
+            pageItem += '<button type="button" class="pagination-button">';
+            pageItem += (i+1);
+            pageItem += '</button>';
+            pageItem += '</li>';
+
+            $('.pagination').append(pageItem);
+        }
+
+        $('.control').css({'margin-left' : -$('.control').width() / 2});
+
+        // 현재 페이지 표시 함수 실행
+        currentPage();
+
+    }
+
+    // 현재 페이지 표시 함수
+    function currentPage(){
+        $('.pagination-item').children('.pagination-button').removeClass('on');
+        $('.pagination-item').eq(currentIndex).children('.pagination-button').addClass('on');
     }
 
     // 초기화 함수 실행
@@ -434,6 +471,15 @@ $(function(){
 
     $('.js-fade-btn.prev').on('click', function(){
         index = currentIndex - 1;
+
+        if( !$('.visual').is(':animated') ){
+            fadePrev();
+        }
+
+    });
+
+    $('.pagination-button').on('click', function(){
+        index = $(this).index('.pagination-button');
 
         if( !$('.visual').is(':animated') ){
             fadePrev();
